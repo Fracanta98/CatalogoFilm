cf99e36002fc9a21d7041231baf81b72ddd92be7
AndreaL9699 <andrea.lattanzio.9@gmail.com>
2025-09-16 18:37:50 +0200

    tentativo sbagliato controller

---
+++ src/main/java/com/example/CatalogoFilm/ControllerFilm.java
@@ -0,0 +1,25 @@
+// import java.lang.reflect.Array;
+// import java.util.ArrayList;
+// import java.util.List;
+
+
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.example.CatalogoFilm.Film;
+// @RestController
+// @RequestMapping("/api")
+  
+
+// public class ControllerFilm {
+//     @PostMapping("/inserimento")
+//     public ArrayList<Film> postFilm(@RequestBody Film f){
+//         new listaFilm.save(f);
+//         return new listaFilm.findAll();
+//     }
+
+// }
