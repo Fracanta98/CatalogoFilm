6ddcd57c31930796a6f8f0ae175bce676b6b7b00
Fracanta98 <thefracanta98@hotmail.com>
2025-09-16 19:31:22 +0200

    aggiunte funzioni nel service

--- src/main/java/com/example/CatalogoFilm/CatalogoFilmService.java
+++ src/main/java/com/example/CatalogoFilm/CatalogoFilmService.java
@@ -1,9 +1,26 @@
 package com.example.CatalogoFilm;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 
 public class CatalogoFilmService {
-   private ArrayList<Film> listaFilm = new ArrayList<>();
+   
+     private final List<Film> listaFilm = new ArrayList<>();
+    private int nextId = 1; //contatore degli id che parte da 1
+
+    public List<Film> getAllFilms() { //metodo per vedere tutti i film disponibili
+        return listaFilm;
+    }
+
+  public Optional<Film> getFilmById(int id) {
+    for (Film f : listaFilm) {
+        if (f.getId() == id) {
+            return Optional.of(f); // Film trovato
+        }
+    }
+    return Optional.empty(); // Nessun film trovato
+    }
     
 }
